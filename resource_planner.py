1c4219183b82d7daafdecde4b72b421087fa491e
Rod Manning <rod.t.manning@gmail.com>
2017-04-08 22:14:26 +1000

    Add parse_args() and get_data() methods
    Added 2 methods/function, get_data() parse_args().
    
    At this stage very basic implementation, but reads the selected excel
    file, and reads it's various sheets into a dict as pandas df's.
    
    Signed-off-by: Rod Manning <rod.manning@taslytic.com>

--- resource_planner.py
+++ resource_planner.py
@@ -5,24 +5,62 @@
 # obtain one at http://mozilla.org/MPL/2.0/.
 
 import pandas as pd
-import maplotlib
-import matplotlib.plot as plt
+import matplotlib
+import matplotlib.pyplot as plt
 import seaborn as sns
+import argparse
 
 
 def parse_args():
     """
     Parse arguments passed to the program.
     """
-    pass
+    parser = argparse.ArgumentParser(
+        description="Resource Planner to determine number of resources available",
+        epilog="Copyright (c) Taslytic Solutions. Licensed under the MPL v2.0",
+        add_help=True,        
+       )
+    parser.add_argument(
+        "--input",
+        metavar="FILE",
+        help="The input file (xlsx-format)",
+        default="data.xlsx",
+        required=True,
+        )
+    parser.add_argument(
+        "--output",
+        metavar="FILE",
+        help="The output file for the charts",
+        default="output.png"
+        )
+    parser.add_argument(
+        "--title",
+        metavar="TEXT",
+        help="Title for the plot",
+        default="Resource Plot"
+        )
+    parser.add_argument(
+        "--subtitle",
+        metavar="TEXT",
+        help="Sub-title for the plot (optional)",
+        default=None,
+        )
+    args = parser.parse_args()
+    return vars(args)
 
 
-def get_data():
+def get_data(path):
     """
     Get data for the program from an input xlsx file.
-    """
-    pass
 
+    path: 
+      The path to the MS Excel (xlsx) file used as input.
+    """
+    data = {}
+    excel_file = pd.ExcelFile(path)
+    for sheet in excel_file.sheet_names:
+        data[sheet.lower()] = excel_file.parse(sheet)
+    return data
 
 def process_data():
     """
@@ -50,6 +88,7 @@ def main():
     Main program control loop.
     """
     args = parse_args()
-
+    data = get_data(args["input"])
+    
 if __name__ == "__main__":
     main()
